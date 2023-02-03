facfd3d3cb292e2eee0f43c458a99e6d594434d8
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-02-03 21:33:02 +0000

    "added codes that grab search input to js"

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+$("#search-button").on("click", function(e){
+    e.preventDefault();
+
+    var cityName = $(".form-input").val();
+    var apiKey = "6b92529c3fbdbf9af10b80a0441ee81b";
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    var historyButton = $("<button>");
+    historyButton.attr("data-name", cityName);
+    historyButton.addClass("historyTab")
+
+    $("#history").append(historyButton);
+    historyButton.style.width = "13px"
+});
+})
