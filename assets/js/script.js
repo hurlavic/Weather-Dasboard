74253534d7afa3a0102fb6d543f7aa64cbb17060
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-02-03 22:54:51 +0000

    "update to js file"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-// Fucntion to fire when search button is clicked
+// Function to trigger when search button is clicked
 $("#search-button").on("click", function(e){
     e.preventDefault();
-
+// Defines variable for search query
     var cityName = $(".form-input").val();
     var apiKey = "6b92529c3fbdbf9af10b80a0441ee81b";
     var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
@@ -17,11 +17,13 @@ $.ajax({
     var button = historyButton.html(cityName);
 
     $("#history").append(button);
-
+// Applies styling to history buttons
     $(".historyTab").css({"border-radius" : "5px", 
     "border" : "1px solid grey", 
     "padding": "5px",
-    "margin-bottom":"8px"})
-    
+    "margin-bottom":"8px"});
+
+    $("#today").text(JSON.stringify(response));
 });
+
 })
