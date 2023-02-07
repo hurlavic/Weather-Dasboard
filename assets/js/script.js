ddf2fa7694809399fb2e783e50332df624736480
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-02-07 22:03:51 +0000

    "fixed forecast data error"

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,17 +45,20 @@ function weatherData(city) {
             console.log(response);
             let forecast = response.daily;
             for (let i = 0; i < 5; i++) {
-                let date = new Date(forecast[i].dt * 1000).toLocaleDateString();
-                let day = new Date(forecast[i].dt * 1000).toLocaleDateString("en-US", { weekday: "long" });
-                let temp = forecast[i].temp.day;
-                let humidity = forecast[i].humidity;
-                let icon = forecast[i].weather[0].icon;
+                let date = new Date(forecast[i+1].dt * 1000).toLocaleDateString();
+                let day = new Date(forecast[i+1].dt * 1000).toLocaleDateString("en-US", { weekday: "long" });
+                let temp = forecast[i+1].temp.day;
+                let wind = forecast[i+1].wind_speed;
+                let windspeed = (wind * 2.237).toFixed(1);
+                let humidity = forecast[i+1].humidity;
+                let icon = forecast[i+1].weather[0].icon;
                 let iconurl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
                 $(`#day${i}`).html(day);
                 $(`#date${i}`).html(date);
-                $(`#temp${i}`).html(temp + "&#8451");
-                $(`#humidity${i}`).html(humidity + "%");
+                $(`#temp${i}`).html("Temp:" + " " + temp + "&#8451");
+                $(`#wind${i}`).html("Wind:" + " " + windspeed + "MPH");
+                $(`#humidity${i}`).html("Humidity:" + " " + humidity + "%");
                 $(`#icon${i}`).html("<img src=" + iconurl + ">");
             }
 
@@ -97,8 +100,9 @@ searchButton.on("click", function(e){
     "padding": "5px",
     "margin-bottom":"8px",
     });
-
-    // 
+// Displays hidden forecast card templates from HTML
+$("#forecast").css({"display" : "inline"});
+$(".forecast-card").css({"background-color" : "#007BFF"})
 })
 // get weather data from clicking search history buttons
 function buttonClick(e){
